e4bf6e5c2dd35b4edfa7ca945cb229f76f8cbafe
joergi <1439809+joergi@users.noreply.github.com>
2024-02-01 23:31:02 +0100

    Adds dependabot.yml for Github Actions

--- script-for-using-latest-version.sh
+++ script-for-using-latest-version.sh
@@ -12,15 +12,16 @@ IFS=$'\n\t'
 # VERSION=0.1
 # you don't need to run this script
 # this is running automatically on Github each day to update the latest issues
-
-TARGET_URL="https://helloworld.raspberrypi.org/"
+TARGET_URL="https://www.raspberrypi.org/hello-world"
 BASEDIR=$(dirname "$0")
 
 #printf -v page_url "$TARGET_URL/issues"
 page_url=$(printf '%s\n' "$TARGET_URL/issues")
 
-latest_issue=$(curl -sf "$page_url" | grep "Download Free PDF" | head -n 1 | sed 's/^.*issues\///' | sed 's/\/pdf.*$//')
-echo "Latest Issue is " "$latest_issue"
+echo $page_url
+latest_issue=$(curl -sf "$page_url" | grep "Download PDF - Issue " | head -n 1 | sed -n 's/.*Download PDF - Issue \(.*\)/\1/p' | sed -n 's/^\([0-9]\+\)".*$/\1/p')
+
+echo "Latest Issue is" "$latest_issue"
 
 file="$BASEDIR/regular-issues.txt";
 echo $file
